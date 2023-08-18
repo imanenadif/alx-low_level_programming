0347f6acd3750cb89b608e9723429177376e97ab
imanenadif <nadifimane43@gmail.com>
2023-08-17 20:00:11 -0700

    push 0x04

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * print_diagonal - print a diagonal line
+ *
+ * @n: is the number of times the \ character
+ *     should be printed
+*/
+
+void print_diagonal(int n)
+{
+	int len, space;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+		{
+			for (space = 0; space < len; space++)
+				_putchar(' ');
+			_putchar('\\'); /*is equal to '/' char*/
+
+			if (len == (n - 1))
+				continue;
+			_putchar('\n');
+		}
+	}
+	putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * print_square - print a square using the character #
+ *
+ * @size: is the size of the square
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_square(int size)
+{
+	int row, column;
+
+	if (size > 0)
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (column = 0; column < (size - 1); column++)
+				_putchar('#');
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
