b0019deb64b4f2246eafb81a78e71587027be691
imanenadif <nadifimane43@gmail.com>
2023-08-28 11:30:25 -0700

    a function that locates a char in a string

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -17,8 +17,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	i = 0;
-	while (i < n)
-		dest[i++] = src[i];
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 	return (dest);
 }
